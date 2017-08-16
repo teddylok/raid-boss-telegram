eabd7c76be3c7e050d6d57ecebeaccf01f4611ea
Daniel Cheng (鄭郁邦) <j16sdiz@users.noreply.github.com>
2017-08-16 18:12:12 +0800

    Update timeslot.ts

--- src/domain/timeslot.ts
+++ src/domain/timeslot.ts
@@ -4,12 +4,12 @@ import * as _ from 'lodash';
 import * as i18n from 'i18next';
 
 export class TimeSlots {
-  getTimeSlots() {
+  getTimeSlots(starttime: any) {
     return [
       { id: 1, text: i18n.t('timeslot.reach'), emoji: Emoji.get('clock3') },
-      { id: 2, text: i18n.t('timeslot.15min'), emoji: Emoji.get('clock330') },
-      { id: 3, text: i18n.t('timeslot.30min'), emoji: Emoji.get('clock930') },
-      { id: 4, text: i18n.t('timeslot.45min'), emoji: Emoji.get('clock9') },
+      { id: 2, text: i18n.t('timeslot.15min') + "(" + starttime.add(15, 'mins').format('HH:mm') + ")", emoji: Emoji.get('clock330') },
+      { id: 3, text: i18n.t('timeslot.30min') + "(" + starttime.add(30, 'mins').format('HH:mm') + ")", emoji: Emoji.get('clock930') },
+      { id: 4, text: i18n.t('timeslot.45min') + "(" + starttime.add(45, 'mins').format('HH:mm') + ")", emoji: Emoji.get('clock9') },
       { id: 5, text: i18n.t('timeslot.reject'), emoji: Emoji.get('no_good') }
     ];
   }
