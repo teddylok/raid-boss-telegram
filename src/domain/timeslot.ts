aa5e9dc7c8fb3f0fe0c752b88c540c545574bf76
Teddy <teddydesu@gmail.com>
2017-08-28 15:30:05 +0800

    update wordings

--- src/domain/timeslot.ts
+++ src/domain/timeslot.ts
@@ -5,15 +5,15 @@ import * as i18n from 'i18next';
 
 export class TimeSlots {
   getTimeSlots(startTime: any) {
-    const after15mins = startTime.add(15, 'mins').format('HH:mm');
-    const after30mins = startTime.add(30, 'mins').format('HH:mm');
-    const after45mins = startTime.add(45, 'mins').format('HH:mm');
+    const after15mins = startTime.add(15, 'minute').format('HH:mm');
+    const after30mins = startTime.add(30, 'minute').format('HH:mm');
+    const after45mins = startTime.add(45, 'minute').format('HH:mm');
 
     return [
       { id: 1, text: i18n.t('timeslot.reach'), emoji: Emoji.get('clock3') },
-      { id: 2, text: i18n.t('timeslot.15min'), emoji: `(${after15mins}) ${Emoji.get('clock330')}` },
-      { id: 3, text: i18n.t('timeslot.30min'), emoji: `(${after30mins}) ${Emoji.get('clock930')}` },
-      { id: 4, text: i18n.t('timeslot.45min'), emoji: `(${after45mins}) ${Emoji.get('clock9')}` },
+      { id: 2, text: i18n.t('timeslot.15min') + `(${after15mins})`, emoji: Emoji.get('clock330') },
+      { id: 3, text: i18n.t('timeslot.30min') + `(${after30mins})`, emoji: Emoji.get('clock930') },
+      { id: 4, text: i18n.t('timeslot.45min') + `(${after45mins})`, emoji: Emoji.get('clock9') },
       { id: 5, text: i18n.t('timeslot.reject'), emoji: Emoji.get('no_good') }
     ];
   }
